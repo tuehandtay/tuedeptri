635da9b23615689531609d35f0b094b58e2b1dd9
HatakekkSheeshh <nguyenquochieujff7@gmail.com>
2024-11-20 11:12:37 +0700

    Update policy2210xxx.py

--- student_submissions/s2210xxx/policy2210xxx.py
+++ student_submissions/s2210xxx/policy2210xxx.py
@@ -13,6 +13,7 @@ class PolicyNetwork(nn.Module):
             nn.Linear(256, 128),
             nn.ReLU(),
             nn.Linear(128, action_dim)
+            len = 0
         )
     
     def forward(self, x):
