a4b207d61ec5618f5b48d22342cf52aec8cf526c
Tuancoolboy <146375320+Tuancoolboy@users.noreply.github.com>
2024-11-20 11:36:55 +0700

    2353280
    change the library

--- student_submissions/s2210xxx/policy2210xxx.py
+++ student_submissions/s2210xxx/policy2210xxx.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from policy import Policy
+
 
 class PolicyNetwork(nn.Module):
     def __init__(self, state_dim, action_dim):
